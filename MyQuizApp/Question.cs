3e8cc234b818871f8e062360d41009961d9125cb
cyrus <cryrus1999coding@gmail.com>
2026-07-15 06:58:09 +0330

    QuizApp, Making the Question Class

---
+++ MyQuizApp/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyQuizApp
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] Answers { get; set; }
+        public int CorrectAnserIndex { get; set; }
+
+        public Question(string questionText, string[] answer, int correctAnserIndex)
+        {
+            QuestionText = questionText;
+            Answers = answer;
+            CorrectAnserIndex = correctAnserIndex;
+        }
+
+        public bool IsCorrectAnswer(int choice)
+        {
+            return CorrectAnserIndex == choice;
+        }
+    }
+}
